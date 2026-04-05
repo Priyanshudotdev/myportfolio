23b12a3a0099369f8b8322bad747454c1f19d055
Priyanshudotdev <priyanshu.kayarkar009@gmail.com>
2026-04-05 15:26:35 +0530

    chore: update dependencies and next config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["vectorlab.in", "i.pinimg.com"],
+    domains: ['vectorlab.in', 'i.pinimg.com', 'dor5tbfyod.ufs.sh'],
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "vectorlab.in",
-        pathname: "/images/Vector.png",
+        protocol: 'https',
+        hostname: 'vectorlab.in',
+        pathname: '/images/Vector.png',
       },
       // You can add more remotePatterns if needed
     ],
